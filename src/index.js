fb29570763c05f95ec115cc5a9de943ee9420a03
ddurumboy2197 <ddurumboy2197@gmail.com>
2026-04-20 15:37:26 +0500

    🚀 Add main application code

---
+++ src/index.js
@@ -0,0 +1,42 @@
+function mergeSortedLists(list1, list2) {
+    let result = [];
+    let i = 0, j = 0;
+
+    while (i < list1.length && j < list2.length) {
+        if (list1[i] < list2[j]) {
+            result.push(list1[i]);
+            i++;
+        } else {
+            result.push(list2[j]);
+            j++;
+        }
+    }
+
+    while (i < list1.length) {
+        result.push(list1[i]);
+        i++;
+    }
+
+    while (j < list2.length) {
+        result.push(list2[j]);
+        j++;
+    }
+
+    return result;
+}
+
+let list1 = [1, 3, 5, 7];
+let list2 = [2, 4, 6, 8];
+
+console.log(mergeSortedLists(list1, list2));
+```
+
+```javascript
+function mergeSortedLists(list1, list2) {
+    return [...list1, ...list2].sort((a, b) => a - b);
+}
+
+let list1 = [1, 3, 5, 7];
+let list2 = [2, 4, 6, 8];
+
+console.log(mergeSortedLists(list1, list2));
